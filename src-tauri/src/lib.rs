56be0cfbc09ed66f83e8ff4c9cac58f34518c10c
mozi1924 <mozi1924@arasaka.ltd>
2026-05-22 03:10:30 +0800

    Add prerelease release workflow for updater builds

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -76,6 +76,7 @@ pub fn run() {
         )
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             tray::reveal_main_window(app);
         }))
